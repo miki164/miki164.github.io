24d45556ab6a75647f05e15d7c53e28fb2a479f5
Jimmy <miki3867@gmail.com>
2021-10-04 19:45:37 +0200

    a

--- src/components/posts-layout.js
+++ src/components/posts-layout.js
@@ -2,22 +2,30 @@ import { graphql } from 'gatsby';
 import React from 'react';
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from './layout';
+import Backlinks from "./backlinks";
 
 export default function PostLayout({ data }) {
-    return (  
+  console.log(data.allMdx.nodes)
+    return (
         <>
-        <Layout>
-        <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        </Layout>
+          <Layout>
+            <MDXRenderer>{data.mdx.body}</MDXRenderer>
+            <Backlinks/>
+          </Layout>
         </>
     );
 }
 
 export const query = graphql`
-query Note($id: String) {
+query Note($id: String, $titleRegex: String) {
   mdx(id: {eq: $id}) {
     id,
     body
+  }
+  allMdx(filter: {rawBody: {regex: $titleRegex}}) {
+    nodes {
+      slug
+    }
   }
 }
 `
